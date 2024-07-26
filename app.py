81d004770b4a02d8076730ab7994afaf75d816f7
LuisWilke <wilke_lopes@hotmail.com>
2024-07-26 15:48:33 -0400

    :wrench: chore

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -7,5 +8,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
