1eadaa43e57a7297a6cc16ad9dc3fafc3af40149
LuisWilke <wilke_lopes@hotmail.com>
2024-07-26 15:37:13 -0400

    :refactor: recycle

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# Definir um rota raiz (pagina inicial) e a função que será executada ao requisitar
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+app.run(debug=True)
